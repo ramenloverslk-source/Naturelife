b5f9bc3a28daf8ad02de3c51943e8115681e303d
ramenloverslk-source <ramenloverslk@gmail.com>
2026-09-11 22:22:08 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.naturlife.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.naturlife.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
