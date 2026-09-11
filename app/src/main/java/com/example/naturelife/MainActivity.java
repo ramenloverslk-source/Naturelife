0a4766bd5e32bd5434c8cb17ee3d5abd8fa61cfa
ramenloverslk-source <ramenloverslk@gmail.com>
2026-09-12 02:43:24 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/naturelife/MainActivity.java
+++ app/src/main/java/com/example/naturelife/MainActivity.java
@@ -1,38 +1,37 @@
 package com.example.naturelife;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // WebView එකක් සාදා Layout එක ලෙස set කිරීම
+        // WebView එකක් code එකෙන්ම සාදා සෙට් කිරීම
         webView = new WebView(this);
         setContentView(webView);
 
-        // WebView Settings සැකසීම
+        // WebView Settings
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
 
-        // App එක ඇතුලෙන්ම Web page එක open වීමට
         webView.setWebViewClient(new WebViewClient());
 
-        // assets/index.html එක Load කිරීම
+        // assets/index.html එක load කිරීම
         webView.loadUrl("file:///android_asset/index.html");
     }
 
-    // Back Button එක එබූ විට WebView එක පිටුපසට යාමට
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
+        if (webView != null && webView.canGoBack()) {
             webView.goBack();
         } else {
             super.onBackPressed();
